e129bce1c531805a5996e5d22234d7a50692033b
ItsL3w1s <9248476+ItsL3w1s@users.noreply.github.com>
2021-08-15 16:58:54 +1000

    edit for personal taste

--- config.js
+++ config.js
@@ -2,36 +2,37 @@ var config = {
 	"ColumnsPerRow": 4,
 
 	"Links": {
-
+		/*
 		"Category": {
 			"Link name": ["https://link-URL.com", "link_icon.ico"],
 			"Another link": ["https://link-URL.com", "png_icon.png"],
 			// Icons are stored in /assets/img/icons/
 		},
+		*/
 
 		"Google": {
-			"Google": ["https://google.co.uk", "google.ico"],
+			"Google": ["https://google.com.au", "google.ico"],
 			"GMail": ["https://gmail.com", "gmail.ico"],
 			"Keep": ["http://keep.google.com/", "keep.ico"],
 			"Translate": ["http://translate.google.com/", "translate.ico"]
 		},
 
 		"Finance": {
-			"PayPal": ["https://www.paypal.com/uk/signin", "paypal.ico"],
-			"MyMonero": ["https://mymonero.com/", "monero.png"]
+			"NAB": ["https://nab.com.au/", "nab.ico"],
+			"PayPal": ["https://www.paypal.com/au/signin", "paypal.ico"]
 		},
 
 		"Shopping": {
-			"Amazon": ["https://smile.amazon.co.uk", "amazon.ico"],
+			"Amazon": ["https://amazon.com.au", "amazon.ico"],
 			"ASOS": ["https://asos.com", "asos.png"],
 			"Steam Store": ["https://store.steampowered.com", "steam.ico"],
-			"Domino's": ["https://dominos.co.uk", "dominos.ico"]
+			"Domino's": ["https://dominos.com.au", "dominos.ico"]
 		},
 
-		"Media": {
-			"Genius": ["https://genius.com/", "genius.png"],
+		"Media/Social": {
+			"Spotify": ["https://spotify.com/", "spotify.png"],
 			"YouTube": ["https://youtube.com", "youtube.ico"],
-			"YouTube Music": ["https://music.youtube.com", "youtube.ico"]
+			"Facebook": ["https://facebook.com", "facebook.png"]
 		},
 
 	}
