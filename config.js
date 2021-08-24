ee4bcdd730f2b47c6d7ed3e993f51e3f4fbd4f03
Lewis <9248476+LightLewis@users.noreply.github.com>
2021-08-24 18:39:21 +1000

    add work related

--- config.js
+++ config.js
@@ -9,31 +9,36 @@ var config = {
 			// Icons are stored in /assets/img/icons/
 		},
 		*/
-
-		"Google": {
-			"Google": ["https://google.com.au", "google.ico"],
-			"GMail": ["https://gmail.com", "gmail.ico"],
-			"Keep": ["http://keep.google.com/", "keep.ico"],
-			"Drive": ["http://drive.google.com/", "drive.png"]
-		},
-
+		
 		"Finance": {
 			"NAB": ["https://nab.com.au/", "nab.ico"],
 			"PayPal": ["https://www.paypal.com/au/signin", "paypal.ico"]
 		},
-
+		
 		"Shopping": {
 			"Menulog": ["https://www.menulog.com.au/", "menulog.ico"],
 			"Amazon": ["https://amazon.com.au", "amazon.ico"],
 			"Steam Store": ["https://store.steampowered.com", "steam.ico"],
 			"Domino's": ["https://dominos.com.au", "dominos.ico"]
 		},
-
+		
 		"Media/Social": {
 			"Spotify": ["https://spotify.com/", "spotify.png"],
 			"Facebook": ["https://facebook.com", "facebook.png"],
 			"Twitter": ["https://twitter.com/home", "twitter.ico"],
 			"YouTube": ["https://youtube.com", "youtube.ico"]
+		},
+
+		"Work": {
+			"Github": ["https://github.com", "github.ico"],
+			"Dashboard": ["https://lewis.marzi.xyz/403.html"]
+		},
+
+		"Google": {
+			"Google": ["https://google.com.au", "google.ico"],
+			"GMail": ["https://gmail.com", "gmail.ico"],
+			"Keep": ["http://keep.google.com/", "keep.ico"],
+			"Drive": ["http://drive.google.com/", "drive.png"]
 		},
 	}
 };
